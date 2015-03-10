a5ca5b8e52090f0f67a4fd4abf33445ffa202938
Vincent Diener <vincent.diener@gmail.com>
2015-03-10 16:27:22 +0100

    Testing writing to DB.

--- index.php
+++ index.php
@@ -6,7 +6,27 @@ require 'Slim/Slim.php';
 $app = new \Slim\Slim();
 
 $app->get('/hello/:name/:bla', function ($name, $bla) {
-    echo "Hello, $name $bla test";
+    echo "Hello, $name $bla";
+	
+	// Get cURL resource
+	$curl = curl_init();
+	// Set some options - we are passing in a useragent too here
+	curl_setopt_array($curl, array(
+		CURLOPT_RETURNTRANSFER => 1,
+		CURLOPT_URL => 'http://docker.teco.edu:8086/db/cool_db/series?u=root&p=root',
+		CURLOPT_USERAGENT => 'Codular Sample cURL Request',
+		CURLOPT_POST => 1,
+		CURLOPT_POSTFIELDS => '[{"name":"foo","columns":["val"],"points":[[23]]}]'
+	));
+	// Send the request & save response to $resp
+	$resp = curl_exec($curl);
+	
+	$info = curl_getinfo($curl);
+	$rsp_code = $info['http_code'];
+	
+	echo "Response: $resp, Code $rsp_code";
+	// Close request to clear up some resources
+	curl_close($curl);
 });
 
 $app->run();
