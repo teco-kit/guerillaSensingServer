1f00222436d1c55b127c960b4124d25efe9e5a76
Vincent Diener <vincent.diener@gmail.com>
2015-03-12 16:24:37 +0100

    Making output pretty for demo purposes.

--- index.php
+++ index.php
@@ -5,26 +5,32 @@ require 'Slim/Slim.php';
 
 $app = new \Slim\Slim();
 
-$app->get('/hello/:name/:bla', function ($name, $bla) {
-    echo "Hello, $name $bla";
+$app->get('/write_data/:value', function ($value) {
+    echo "<b>Writing $value to table 'data' in database 'test_db'.<br><br></b>";
 	
 	// Get cURL resource
 	$curl = curl_init();
 	// Set some options - we are passing in a useragent too here
 	curl_setopt_array($curl, array(
 		CURLOPT_RETURNTRANSFER => 1,
-		CURLOPT_URL => 'http://docker.teco.edu:8086/db/cool_db/series?u=root&p=root',
+		CURLOPT_URL => 'http://docker.teco.edu:8086/db/test_db/series?u=root&p=root',
 		CURLOPT_USERAGENT => 'Codular Sample cURL Request',
 		CURLOPT_POST => 1,
-		CURLOPT_POSTFIELDS => '[{"name":"foo","columns":["val"],"points":[[23]]}]'
+		CURLOPT_POSTFIELDS => '[{"name":"data","columns":["val"],"points":[[' . $value . ']]}]'
 	));
 	// Send the request & save response to $resp
 	$resp = curl_exec($curl);
 	
 	$info = curl_getinfo($curl);
 	$rsp_code = $info['http_code'];
+	echo "<table style=\"border: 1px solid black;\">";
+	if (empty($resp)){
+		echo "<tr><td>Response:</td><td>All OK.</td></tr><tr><td>Code:</td><td>$rsp_code</td></tr>";
+	} else {
+		echo "<tr><td>Response:</td><td>$resp</td></tr><tr><td>Code:</td><td>$rsp_code</td></tr>";
+	}
+	echo "</table>";
 	
-	echo "Response: $resp<br>Code $rsp_code";
 	// Close request to clear up some resources
 	curl_close($curl);
 });
