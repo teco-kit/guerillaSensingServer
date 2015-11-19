14f8d3cfc36ec42cd3b9d209ec716d484f834f7e
vince-d <vincent.diener@gmail.com>
2015-11-19 18:53:21 +0100

    Added IOSB query endpoints.

--- index.php
+++ index.php
@@ -12,6 +12,10 @@ $app->post('/add_device/', 'add_device');
 $app->post('/write_data/', 'write_data');
 $app->post('/check_uuids/', 'check_uuids');
 
+// Frauenhofer IOSB
+$app->post('/iosb_write_data/', 'iosb_write_data');
+$app->get('/iosb_read_data/:query', 'iosb_read_data');
+
 // Test API to write some arbitrary data into the table "data" in "test_db".
 function test_write_data($value) {
     echo "<b>Writing $value to table 'data' in database 'test_db'.<br><br></b>";
@@ -112,6 +116,134 @@ function add_device() {
 	//$result = array("mac"=>$nd_mac,"name"=>$nd_name);
 	$result = "Code: " . $rsp_code . ". Device " . $nd_mac . " added.";
 	echo json_encode($result);
+}
+
+// Write IOSB data to the IOSB DB.
+function iosb_write_data() {
+	// Read parameters from POST body and collect data.
+	$app = \Slim\Slim::getInstance();
+	$request = $app->request();
+	$body = $request->getBody();
+	$input_array = json_decode($body, true); 
+	
+	$curl = curl_init();
+	$db = 'iosb_demo';
+	$table = 'iosb_table';
+
+	$rsp_code = 0;
+	$resp = "";
+	
+	foreach ($input_array as $input) {
+	
+		// Upload UUID of of this data point.
+		// Data that is uploaded together will have the same upload UUID.
+		$data_uuid = $input["id"];
+		
+		// Timestamp
+		$data_time = intval($input["time"]);
+		// MAC of device that provided the data.
+		$data_mac = $input["mac"];
+		// Height of device.
+		$data_height = $input["height"];
+		// Latitude of device.
+		$data_lat = $input["lat"];
+		// Longitude level of device.
+		$data_lon = $input["lon"];
+		// Value of sensor 1. (Temperature)
+		$data_temp = $input["temp"];
+		// Value of sensor 2. (Humidity)
+		$data_hum = $input["hum"];
+		// Value of sensor 3. (CO2)
+		$data_co2 = $input["co2"];
+		// Value of sensor 4. (CO)
+		$data_co = $input["co"];
+		// Value of sensor 5. (NO2)
+		$data_no2 = $input["no2"];
+		// Value of sensor 6. (O3)
+		$data_o3 = $input["o3"];
+		// Value of sensor 7. (Fine dust)
+		$data_dust = $input["dust"];
+		// Value of sensor 8. (UV)
+		$data_uv = $input["uv"];
+		
+		
+		// Done collecting data. Now write it to the TSDB.
+		curl_setopt_array($curl, array(
+			CURLOPT_RETURNTRANSFER => 1,
+			CURLOPT_URL => 'http://docker.teco.edu:8086/db/' . $db . '/series?u=root&p=root',
+			CURLOPT_USERAGENT => 'GuerillaSensingPHPServer',
+			CURLOPT_POST => 1,
+			CURLOPT_POSTFIELDS => '[{"name":"' . $table . '",
+									"time_precision":"ms",
+									"columns":["time","mac","height","lat","lon","temp","hum","co2","co","no2","o3","dust","uv"],
+									"points":[[' . $data_time . ',"' . $data_mac . '","' . $data_height . '",
+											   "' . $data_lat . '","' . $data_lon . '", 
+											   "' . $data_temp . '","' . $data_hum . '",
+											   "' . $data_co2 . '","' . $data_co . '",
+											   "' . $data_no2 . '","' . $data_o3 . '",
+											   "' . $data_dust . '","' . $data_uv . '"]]}]'
+		));
+		
+		// Send the request & save response to $resp
+		$resp = curl_exec($curl);
+		
+		// Read response.
+		$info = curl_getinfo($curl);
+		$rsp_code = $info['http_code'];	
+	}
+	
+	// Close request to clear up some resources.
+	curl_close($curl);
+	
+	// No error handling yet. Just print back results.
+	$result = "Code: " . $rsp_code . ". Data written.";
+	echo json_encode($result);	
+}
+
+// Write IOSB data to the IOSB DB.
+function iosb_read_data($query) {
+	// Get cURL resource.
+	$curl = curl_init();
+	
+	$app = \Slim\Slim::getInstance();
+	
+	// URL-encode query.
+	$query_url = urlencode($query);
+	
+	// Set some options.
+	curl_setopt_array($curl, array(
+		CURLOPT_RETURNTRANSFER => 1,
+		CURLOPT_URL => 'http://docker.teco.edu:8086/db/iosb_demo/series?q=' . $query_url . '&u=root&p=root',
+		CURLOPT_USERAGENT => 'GuerillaSensingPHPServer'
+	));
+	
+	// Send the request & save response to $resp
+	$resp = curl_exec($curl);
+	
+	// Create table with written data and show it back to user.
+	$info = curl_getinfo($curl);
+	$rsp_code = $info['http_code'];
+	
+	// Close request to clear up some resources
+	curl_close($curl);
+	
+	// If response code is not 200, the database might be down.
+	if ($rsp_code == 200) {
+		// Directly return JSON from server.
+		echo $resp;
+	} else {
+		$app->response->setStatus(404);
+		echo("Error: cURL returned $rsp_code");
+		
+		$msg = "The GuerillaSensing database seems to be offline.\nUser got response code $rsp_code";
+
+		// use wordwrap() if lines are longer than 70 characters
+		$msg = wordwrap($msg, 70);
+
+		// send email
+		// mail("diener@teco.edu", "GuerillaSensing database issues", $msg);
+		exec("echo \"From: teco <noreply@teco.edu>\nTo: diener <diener@teco.edu>\nSubject: Error\n\nThe database seems to be down.\" | msmtp --debug -a gmail diener@teco.edu");
+	}
 }
 
 // Write sensor data to the TSDB.
